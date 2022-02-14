528f38245b3f0cd6ef424aaa598f61e8713c57d3
Alvaro Zaera <alo.zaera@gmail.com>
2022-02-14 19:59:14 +0100

    Packages need solution

---
+++ P1/FrioCalor.jl
@@ -0,0 +1,111 @@
+include("RedNeuronal.jl")
+using .RedNeuronal_pkg
+include("Capa.jl")
+using .Capa_pkg
+include("Neurona.jl")
+using .Neurona_pkg
+
+
+using ArgParse
+using DelimitedFiles
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--input_file"
+            help = "Fichero con los valores de entrada para la red frio-calor."
+            required = true
+        "--output_file"
+            help = "Fichero en el que se van a almacenar los valores de la neurona en cada instante de tiempo."
+            required = true
+    end
+
+    return parse_args(s)
+end
+
+function crear_red_frio_calor()
+    red = RedNeuronal_pkg.Crear()
+
+    x1 = Neurona_pkg.Crear(1.0, Neurona_pkg.Directa)
+    x2 = Neurona_pkg.Crear(1.0, Neurona_pkg.Directa)
+    capa_entrada = Capa_pkg.Crear()
+    Capa_pkg.Añadir(capa_entrada, x1)
+    Capa_pkg.Añadir(capa_entrada, x2)
+    RedNeuronal_pkg.Añadir(red, capa_entrada)
+
+    z1 = Neurona_pkg.Crear(2.0, Neurona_pkg.McCulloch)
+    z2 = Neurona_pkg.Crear(2.0, Neurona_pkg.McCulloch)
+    capa_oculta = Capa_pkg.Crear()
+    Capa_pkg.Añadir(capa_oculta, z1)
+    Capa_pkg.Añadir(capa_oculta, z2)
+    RedNeuronal_pkg.Añadir(red, capa_oculta)
+
+    y1 = Neurona_pkg.Crear(2.0, Neurona_pkg.McCulloch)
+    y2 = Neurona_pkg.Crear(2.0, Neurona_pkg.McCulloch)
+    capa_salida = Capa_pkg.Crear()
+    Capa_pkg.Añadir(capa_salida, y1)
+    Capa_pkg.Añadir(capa_salida, y2)
+    RedNeuronal_pkg.Añadir(red, capa_salida)
+
+    Neurona_pkg.Conectar(x1, y1, 2.0)
+    #
+    Neurona_pkg.Conectar(x2, z1, -1.0)
+    Neurona_pkg.Conectar(x2, z2, 2.0)
+    Neurona_pkg.Conectar(x2, y2, 1.0)
+    #
+    Neurona_pkg.Conectar(z1, y1, 2.0)
+    #
+    Neurona_pkg.Conectar(z2, z1, 2.0)
+    Neurona_pkg.Conectar(z2, y2, 1.0)
+
+    return red, x1, x2
+end
+
+
+function main()
+    
+    #parsed_args = parse_commandline()
+
+    #input_file = parsed_args["input_file"]
+    #output_file = parsed_args["output_file"]
+    input_file = "input.txt"
+    output_file = "output-txt"
+
+    red, x1, x2 = crear_red_frio_calor()
+    valores = Array(["x1", "x2", "z1", "z2", "y1", "y2"])
+
+    for line in readlines(input_file)
+        calor, frio = split(line, " ")
+        Neurona_pkg.Inicializar(x1, calor)
+        Neurona_pkg.Inicializar(x2, frio)
+        RedNeuronal_pkg.Disparar(red)
+        RedNeuronal_pkg.Inicializar(red)
+        RedNeuronal_pkg.Propagar(red)
+
+        push!(valores, [neurona.valor_salida for capa in red.capas for neurona in capa.neuronas])
+    end
+
+    # TODO: Comentar
+    Neurona_pkg.Inicializar(x1, 0)
+    Neurona_pkg.Inicializar(x2, 0)
+    RedNeuronal_pkg.Disparar(red)
+    RedNeuronal_pkg.Inicializar(red)
+    RedNeuronal_pkg.Propagar(red)
+    push!(valores, [neurona.valor_salida for capa in red.capas for neurona in capa.neuronas])
+
+    Neurona_pkg.Inicializar(x1, 0)
+    Neurona_pkg.Inicializar(x2, 0)
+    RedNeuronal_pkg.Disparar(red)
+    RedNeuronal_pkg.Inicializar(red)
+    RedNeuronal_pkg.Propagar(red)
+    push!(valores, [neurona.valor_salida for capa in red.capas for neurona in capa.neuronas])
+    
+
+    writedlm(output_file, valores)
+
+    RedNeuronal_pkg.Liberar(red)
+
+end
+
+main()
---
+++ P1/includeFunctions.jl
@@ -0,0 +1,3 @@
+push!(LOAD_PATH,"RedNeuronal.jl")
+push!(LOAD_PATH,"Capa.jl")
+push!(LOAD_PATH,"Neurona.jl")
